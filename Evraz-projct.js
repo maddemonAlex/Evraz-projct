772eec84981ab81d93ba267e374a28ecc744e717
maddemonAlex <chirkin775@gmail.com>
2025-05-21 17:59:31 +0300

    Проэкт день 10

--- Evraz-projct.js
+++ Evraz-projct.js
@@ -10,5 +10,11 @@ function accident(id, dotId,svgId ){
     document.getElementById(svgId).classList.add('svg-accident')
     document.getElementById(id).classList.remove('vent-control-active')
 }
+function service(id, dotId,svgId ){
+    document.getElementById(id).classList.add('vent-control-service')
+    document.getElementById(dotId).classList.add('dot-service')
+    document.getElementById(svgId).classList.add('svg-service')
+    document.getElementById(id).classList.remove('vent-control-active')
+}
 activateVent('П1',"dot1","svg1")
 accident('П3','dot3','svg3')
