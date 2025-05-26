424ae4984cef03a0ab549851f2194cd2f5cc60fe
maddemonAlex <chirkin775@gmail.com>
2025-05-26 15:56:41 +0300

    Проэкт день 9

--- Evraz-projct.js
+++ Evraz-projct.js
@@ -1,20 +1,57 @@
-function activateVent(id, dotId,svgId ){
+function activateVent(id, dotId, svgId, temp) {
     document.getElementById(id).classList.add('vent-control-active')
     document.getElementById(dotId).classList.add('dot-active')
     document.getElementById(svgId).classList.add('svg-active')
     document.getElementById(id).classList.remove('vent-control-accident')
+    document.getElementById(id).classList.remove('vent-control-service')
 }
-function accident(id, dotId,svgId ){
+
+function accident(id, dotId, svgId) {
     document.getElementById(id).classList.add('vent-control-accident')
     document.getElementById(dotId).classList.add('dot-accident')
     document.getElementById(svgId).classList.add('svg-accident')
     document.getElementById(id).classList.remove('vent-control-active')
 }
-function service(id, dotId,svgId ){
+
+function service(id, dotId, svgId) {
     document.getElementById(id).classList.add('vent-control-service')
     document.getElementById(dotId).classList.add('dot-service')
     document.getElementById(svgId).classList.add('svg-service')
     document.getElementById(id).classList.remove('vent-control-active')
+    document.getElementById(id).classList.remove('vent-control-accident')
 }
-activateVent('П1',"dot1","svg1")
-accident('П3','dot3','svg3')
+
+
+function moveTo(targetNumber, time, id, dotId) {
+    const el = document.getElementById(id)
+    let interval = setInterval(gMoneyU1, time);
+    let curentTemp = +el.innerHTML;
+    const delta = targetNumber - curentTemp;
+
+    function gMoneyU1() {
+        if (delta > 0) {
+        document.getElementById(dotId).classList.add('flame-control-active')
+        document.getElementById(dotId).classList.remove('flame_control')
+    }
+        curentTemp += (delta / time);
+        if (delta > 0 && curentTemp >= targetNumber) {
+            curentTemp = targetNumber;
+            console.log(9999)
+            document.getElementById(dotId).classList.add('flame_control')
+            document.getElementById(dotId).classList.remove('flame_control-active')
+            clearInterval(interval)
+        }
+        if (delta < 0 && curentTemp <= targetNumber) {
+            curentTemp = targetNumber;
+            clearInterval(interval)
+        }
+        el.innerHTML = curentTemp.toFixed(1);
+
+    }
+}
+
+moveTo(30, 100, 'temp3', 'flame3')
+
+activateVent('П1', "dot1", "svg1")
+accident('П3', 'dot3', 'svg3')
+service("П2", 'dot2', 'svg2')
